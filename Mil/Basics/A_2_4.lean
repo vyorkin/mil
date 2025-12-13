8b97f3cb9ae0f7a78d71ba15e3bf7f434a801fa2
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-12-13 11:45:56 +0300

    Update

--- Mil/Basics/A_2_4.lean
+++ Mil/Basics/A_2_4.lean
@@ -8,11 +8,11 @@ variable (a b c d e f x y w z : ℝ)
 #check (min_le_right a b : min a b ≤ b)
 #check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
 
-#check (lt_min_iff : a < min b c ↔ a < b ∧ a < c)
-#check (min_lt_iff : min a b < c ↔ a < c ∨ b < c)
+#check (lt_min_iff : a < min b c ↔ (a < b) ∧ (a < c))
+#check (min_lt_iff : min a b < c ↔ (a < c) ∨ (b < c))
 
-#check (le_min_iff : c ≤ min a b ↔ c ≤ a ∧ c ≤ b)
-#check (min_le_iff : min a b ≤ c ↔ a ≤ c ∨ b ≤ c)
+#check (le_min_iff : c ≤ min a b ↔ (c ≤ a) ∧ (c ≤ b))
+#check (min_le_iff : min a b ≤ c ↔ (a ≤ c) ∨ (b ≤ c))
 
 #check (min_le_min : a ≤ c → b ≤ d → min a b ≤ min c d)
 
@@ -22,7 +22,7 @@ variable (a b c d e f x y w z : ℝ)
 #check (min_eq_left_iff : min a b = a ↔ a ≤ b)
 #check (min_eq_right_iff : min a b = b ↔ b ≤ a)
 
-#check (min_eq_iff : min a b = c ↔ a = c ∧ a ≤ b ∨ b = c ∧ b ≤ a)
+#check (min_eq_iff : min a b = c ↔ (a = c ∧ a ≤ b) ∨ (b = c ∧ b ≤ a))
 
 #check (le_max_left a b : a ≤ max a b)
 #check (le_max_right a b : b ≤ max a b)
@@ -128,10 +128,50 @@ theorem aux : min a b + c ≤ min (a + c) (b + c) := by
 example : min a b + c = min (a + c) (b + c) := by
   apply le_antisymm
   · apply aux
+  · have h : min (a + c) (b + c) =
+             min (a + c) (b + c) - c + c := by rw [sub_add_cancel]
+    rw [h]
+    apply add_le_add_right
+    rw [sub_eq_add_neg]
+    apply le_trans
+    · apply aux
+    · rw [add_neg_cancel_right, add_neg_cancel_right]
+
+-- Моя следующая фундаментальная ошибка в том, что
+-- я перестаю пользоваться интуицией когда перехожу к формальному доказательству.
+-- Я перестаю видеть смысл за символами и начинаю оперировать ими
+-- согласно установленному множеству правил-теорем, используя этакий
+-- шизофреничный способ мышления перебором вариантов.
+-- (Объяснение ниже - хуета?)
+-- В начале должно быть интуитивное глубокое понимание формулировки
+-- теоремы и некоторая идея доказательства. Например, увидеть,
+-- что в теореме ниже с двух сторон по сути:
+--
+-- min a b = a                                      a + c = min (a + c) (b + c)
+--     ∨        => {a | b} + c = {a + c | b + c} <=
+-- min a b = b                                      b + c = min (a + c) (b + c)
+--
+-- Бля, как будто этот аски-арт только путает, но похуй.
+--
+-- Затем надо понять, что это похоже на хвост
+-- add_le_add_right : a ≤ b → ∀ c, a + c ≤ b + c
+-- И начать преобразовывать к этому виду другую сторону неравенства.
+-- Да, на несколько шагов вперёд ты в голове не можешь это осмыслить,
+-- у тебя тупо не хватит ОЗУ, но ты можешь сделать шаг и посмотреть на ситуацию, в
+-- в которую он тебя приведёт: Тупик или нет? Как двигаться дальше? Чем можешь воспользоваться?
+--
+-- Короче, тут тонкая грань:
+-- Идея первого шага может быть похожа на хвост той теоремы,
+-- с помощью которой можно начать обратное рассуждение.
+-- Это похоже на подход, когда ты перебираешь варианты, но "это другое".
+
+example : min a b + c = min (a + c) (b + c) := by
+  apply le_antisymm
+  · apply aux
   rw [← add_neg_cancel_right (min (a + c) (b + c)) c]
   rw [add_assoc, ← add_comm (-c), ← add_assoc]
   apply add_le_add_right
-  apply le_min
+  apply le_min -- (!1)
   · nth_rw 2 [← add_neg_cancel_right a (-c)]
     rw [neg_neg]
     rw [add_assoc, add_comm (-c), ← add_assoc]
@@ -142,6 +182,25 @@ example : min a b + c = min (a + c) (b + c) := by
   rw [add_assoc, add_comm (-c), ← add_assoc]
   apply add_le_add_right
   apply min_le_right
+
+-- Я доказал, но доказательство выглядит ужасающе.
+-- Вот чего я не увидел:
+--
+-- 1) Снова не заметил что вычитание это сложение, поэтому на шаге (!1)
+--    применил le_min вместо rw [sub_eq_add_neg]. Дальше всё могло быть (и было бы) иначе.
+-- 2) Не нашёл sub_add_cancel : a - b + b = a,
+--    вместо неё использовал неудобную add_neg_cancel_right.
+--    Я её не нашёл потому, что просто не искал, а использовал только те,
+--    которые мне встречались ранее по мере прохождения книги.
+-- 3) Недооцениваю введение промежуточных гипотез.
+--    Что проще понятнее выглядит / проще читается?
+--    а)
+--      rw [← add_neg_cancel_right (min (a + c) (b + c)) c]
+--      rw [add_assoc, ← add_comm (-c), ← add_assoc]
+--    b)
+--      have h : min (a + c) (b + c) =
+--               min (a + c) (b + c) - c + c := by rw [sub_add_cancel]
+--      rw [h]
 
 -- https://github.com/avigad/mathematics_in_lean_source/issues/348
 -- #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
