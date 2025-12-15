281feaffaa419cbf88f8a5637c858747a2669f87
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-12-15 09:35:03 +0300

    Update

--- Mil/Logic/A_3_1.lean
+++ Mil/Logic/A_3_1.lean
@@ -39,11 +39,13 @@ variable (a b δ : ℝ)
 variable (h₀ : 0 < δ) (h₁ : δ ≤ 1)
 variable (ha : |a| < δ) (hb : |b| < δ)
 
-#check my_lemma2 h₀ h₁
+-- Теперь можно не писать a b δ.
 #check my_lemma2 h₀ h₁ ha hb
+-- Можно частично применить только к паре гипотез-аргументов.
+#check my_lemma2 h₀ h₁
 end
 
-end My1Gkkj
+end My1
 
 namespace My2
 
@@ -66,7 +68,6 @@ theorem my_lemma3 :
 
 -- Завершить доказательство, используя следующие теоремы:
 -- abs_mul, mul_le_mul, abs_nonneg, mul_lt_mul_right, and one_mul.
--- Этих 5 теорем достаточно или это подъёбка?
 theorem my_lemma4' :
     ∀ {x y ε : ℝ}, 0 < ε → ε ≤ 1 → |x| < ε → |y| < ε → |x * y| < ε := by
   intro x y ε epos ele1 xlt ylt
@@ -155,3 +156,210 @@ theorem my_lemma4 :
     _ = ε := one_mul ε
 
 end My2
+
+namespace My3
+
+def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, f x ≤ a
+
+def FnLb (f : ℝ → ℝ) (a : ℝ) : Prop :=
+  ∀ x, a ≤ f x
+
+section
+variable (f g : ℝ → ℝ) (a b : ℝ)
+
+example (hfa : FnUb f a) (hgb : FnUb g b)
+        : FnUb (fun x ↦ f x + g x) (a + b) := by
+  -- Раскроет определение FnUb потому, что внутри него ∀ x.
+  intro x
+  -- Эта тактика упрощает "по определению".
+  -- В нашем случае упрощает лямбду, выполняя подстановку.
+  dsimp
+  -- Тоже самое можно было бы сделать так:
+  -- change f x + g x ≤ a + b
+
+  -- (h₁ : a ≤ b) (h₂ : c ≤ d) : a + c ≤ b + d
+  apply add_le_add
+  · apply hfa
+  · apply hgb
+
+-- Упражнения.
+
+example (hfa : FnLb f a) (hgb : FnLb g b)
+  : FnLb (fun x ↦ f x + g x) (a + b) := by
+  intro x
+  dsimp
+  apply add_le_add
+  · apply hfa
+  · apply hgb
+
+example (nnf : FnLb f 0) (nng : FnLb g 0)
+  : FnLb (fun x ↦ f x * g x) 0 := by
+  intro x
+  dsimp
+  apply mul_nonneg
+  · apply nnf
+  · apply nng
+
+example (hfa : FnUb f a) (hgb : FnUb g b) (nng : FnLb g 0) (nna : 0 ≤ a)
+  : FnUb (fun x ↦ f x * g x) (a * b) := by
+  intro x; dsimp
+  apply mul_le_mul
+  · apply hfa
+  · apply hgb
+  · apply nng
+  · apply nna
+
+end
+
+end My3
+
+namespace My4
+
+-- Если мы докажем одну из теорем выше не просто для ℝ, а для набора тайпклассов,
+-- то она будет работать для типов, которые являются их инстансами.
+
+variable {α : Type*} {R : Type*}
+  [AddCommMonoid R] [PartialOrder R]
+  [IsOrderedCancelAddMonoid R]
+
+#check add_le_add -- {α : Type u} [Add α] [Preorder α] [AddLeftMono α] [AddRightMono α]
+--                   {a b c d : α} (h₁ : a ≤ b) (h₂ : c ≤ d) : a + c ≤ b + d
+
+def FnUb (f : α → R) (a : R) : Prop := ∀ x, f x ≤ a
+
+-- Теорема add_le_add принимает (h₁ : a ≤ b) и (h₂ : c ≤ d).
+-- Мы её их и передаём, только у нас они завёрнуты в определения
+-- h₁ := FnUb f a = f x ≤ a
+-- h₂ := FbUb g b = g x ≤ b
+
+theorem fnUb_add {f g : α → R} {a b : R}
+                 (hfa : FnUb f a) (hgb : FnUb g b)
+                : FnUb (fun x ↦ f x + g x) (a + b) :=
+  fun x ↦ add_le_add (hfa x) (hgb x)
+
+end My4
+
+namespace My5
+
+-- def Monotone (f : α → β) : Prop :=
+--   ∀ ⦃a b⦄, a ≤ b → f a ≤ f b
+
+-- Определение Monotone f в точности соответствует тому,
+-- что написано справа от `:`.
+example (f : ℝ → ℝ) (h : Monotone f)
+        : ∀ {a b}, a ≤ b → f a ≤ f b := @h
+
+section
+
+variable (f g : ℝ → ℝ)
+
+example (mf : Monotone f) (mg : Monotone g) : Monotone (fun x ↦ f x + g x) := by
+  -- Разворачивает отпределения
+  unfold Monotone
+  -- Хотя intro тоже развернуло бы
+  intro a b -- a ≤ b → (fun x ↦ f x + g x) a ≤ (fun x ↦ f x + g x) b
+  dsimp     -- a ≤ b → f a + g a ≤ f b + g b
+  intro h   -- f a + g a ≤ f b + g b
+  apply add_le_add -- (h₁ : a ≤ b) (h₂ : c ≤ d) : a + c ≤ b + d
+  · apply mf h
+  · apply mg h
+
+-- Когда доказательство настолько тривиальное, то лучше сразу
+-- сконструировать терм, а не доказывать в тактик-мод.
+example (mf : Monotone f) (mg : Monotone g) : Monotone (fun x ↦ f x + g x) :=
+  fun _a _b h ↦ add_le_add (mf h) (mg h)
+
+end
+
+end My5
+
+namespace My6
+
+-- Упражнения.
+
+variable (f g : ℝ → ℝ)
+
+#check mul_le_mul_of_nonneg_left -- (h : b ≤ c) (a0 : 0 ≤ a) : a * b ≤ a * c
+
+example {c : ℝ} (mf : Monotone f) (nnc : 0 ≤ c) : Monotone fun x ↦ c * f x := by
+  intro a b h; dsimp
+  show c * f a ≤ c * f b
+  -- (h : b ≤ c) (a0 : 0 ≤ a) : a * b ≤ a * c
+  apply mul_le_mul_of_nonneg_left
+  · exact mf h
+  · assumption
+
+example {c : ℝ} (mf : Monotone f) (nnc : 0 ≤ c) : Monotone fun x ↦ c * f x :=
+  fun _ _ h => mul_le_mul_of_nonneg_left (mf h) nnc
+
+example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) := by
+  intro a b h; dsimp
+  apply mf
+  apply mg
+  exact h
+
+example (mf : Monotone f) (mg : Monotone g) : Monotone fun x ↦ f (g x) :=
+  fun _ _ h => mf (mg h)
+
+end My6
+
+namespace My7
+
+-- Функция называется чётной,   если ∀ x, f( x) =  f(-x)
+--
+-- Функция называется нечётной, если ∀ x, f(-x) = -f( x)
+--                    или тоже самое ∀ x, f( x) = -f(-x)
+--
+-- https://en.wikipedia.org/wiki/Even_and_odd_functions
+
+def FnEven (f : ℝ → ℝ) : Prop := ∀ x, f x = f (-x)
+def FnOdd (f : ℝ → ℝ) : Prop := ∀ x, f x = -f (-x)
+
+example (ef : FnEven f) (eg : FnEven g)
+        : FnEven fun x ↦ f x + g x := by
+  intro x
+  -- Можешь раскрывать определения, если тебе так проще.
+  unfold FnEven at ef
+  unfold FnEven at eg
+  calc
+    (fun x ↦ f x + g x) x = f x + g x := rfl
+    _ = f (-x) + g (-x) := by rw [ef, eg]
+
+-- Упражнения.
+
+-- Если обе ф-ции нечётные, то их произведение – чётная функция.
+example (of : FnOdd f) (og : FnOdd g) : FnEven fun x ↦ f x * g x := by
+  intro a
+  -- Без dsimp не получится переписать rw [of, og].
+  -- Лямбда абстракция не даст это сделать, потому что не получится
+  -- найти термы вида f x и g x в выражении цели.
+  -- Зацени:
+  -- (fun x ↦ f x * g x)
+  -- Видишь они там есть, но они зависят от конкретного x,
+  -- которых тут - связанная переменная. Т.е. это не любой x.
+  -- Тактика rw (rewrite) работает на синтаксическом уровне и она
+  -- не будет разворачивать определения или редуцировать термы.
+  -- Есть тактика erw, которая делает в этом смысле чуть больше,
+  -- чем rw, но не намного.
+  dsimp
+  unfold FnOdd at of og
+  rw [of, og]
+  rw [neg_mul_neg]
+
+-- Произведение чётной и нечётной функций - нечётная функция.
+example (ef : FnEven f) (og : FnOdd g) : FnOdd fun x ↦ f x * g x := by
+  intro a; dsimp;
+  unfold FnEven at ef; unfold FnOdd at og
+  rw [ef, og]
+  rw [← mul_neg]
+
+-- Композиция (последовательное применение) нечётной и чётной функций – чётная функция.
+example (ef : FnEven f) (og : FnOdd g) : FnEven fun x ↦ f (g x) := by
+  intro a; dsimp
+  unfold FnEven at ef; unfold FnOdd at og
+  rw [ef, og, neg_neg]
+
+-- Когда научишься замечать кванторы существования – ты будешь находить их везде.
+
+end My7
